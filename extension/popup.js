f5b1d8d35f39fe1b9d9eb03b849ad432ec7c6cc0
Krzysztof <krzysztof.gebka2001@gmail.com>
2024-09-28 21:35:19 +0200

    getting current url

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,11 @@
 document.getElementById('helloBtn').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'Hello, this is your first extension!';
-});  
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const currentTab = tabs[0];
+        if (currentTab) {
+            document.getElementById('message').textContent = currentTab.url;
+          } else {
+            document.getElementById('message').textContent = 'Unable to get URL';
+          }
+        });    
+    }
+);
