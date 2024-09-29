a576205758c791b61740fcb9bb6a18c46bc43c2c
nonelone <nonelpublic@proton.me>
2024-09-29 11:11:24 +0200

    Don't try to be smart if you are stupid

--- api.py
+++ api.py
@@ -9,7 +9,35 @@ from models import verify_url
 # register blueprint
 api_blueprint = Blueprint('api', __name__)
 
+
 @api_blueprint.route("/api", methods=["POST"])
+def api():
+    if request.method == 'POST':
+        if request.form['url'] is not None:
+            requested_url = request.form['url']
+            url = urlparse(requested_url)
+            if url.scheme == "http": # if the protocol is http
+                new_url = urlunparse(url._replace(scheme="https"))
+                try:
+                    with requests.get(new_url) as r: # try to connect over https
+                        if r.status_code == 200: # if it is possible we replace the url
+                            url = new_url
+                except: pass
+                return redirect(url_for('website.protected', reason="insecure"))
+            if url.scheme != "https":
+                return redirect(url_for('website.protected', reason="bad_protocol"))
+
+            secure = verify_url(url.netloc)
+            if secure:
+                return redirect(url_for('website.protected', reason="success"),code=307)
+            else:
+                return redirect(url_for('website.protected', reason="dangerous"))
+        else: abort(503)
+
+    elif request.method == 'GET':
+        return '<title>API</title><h1>For API usage, dummy!</h1>'
+    abort(403)
+
 def api():
     if request.method == 'POST':
         if request.form['url'] is not None: # if method is POST and specifies `url`
